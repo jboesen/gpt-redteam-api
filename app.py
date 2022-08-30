5ca514ea17a64c4c2d7aead95a17e04043738fe1
John Boesen <89944638+jboesen@users.noreply.github.com>
2022-08-30 00:52:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,25 +2,25 @@
 import os
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-# from generate_text import get_aggregated_completions
-# from offensive_classifier import sort_offensive
+from generate_text import get_aggregated_completions
+from offensive_classifier import sort_offensive
 
 app = Flask(__name__)
 CORS(app)
 
-# @app.route('/', methods=['POST'])
-# def predictions_and_offensiveness():
-#     """Sorts predictions by offensiveness for redteaming"""
-#     req = request.get_json()
-#     generated = get_aggregated_completions(req['prompt'], req['numPredictions'])
-#     sorted_greedy = sort_offensive(generated['greedy'])
-#     sorted_beam = sort_offensive(generated['beam'])
-#     return_dict = {
-#         'attention': generated['attention'].tolist(),
-#         'beam': sorted_beam,
-#         'greedy': sorted_greedy,
-#         'tokens': [token.replace('Ġ', '') for token in generated['tokens']]}
-#     return jsonify(return_dict)
+@app.route('/', methods=['POST'])
+def predictions_and_offensiveness():
+    """Sorts predictions by offensiveness for redteaming"""
+    req = request.get_json()
+    generated = get_aggregated_completions(req['prompt'], req['numPredictions'])
+    sorted_greedy = sort_offensive(generated['greedy'])
+    sorted_beam = sort_offensive(generated['beam'])
+    return_dict = {
+        'attention': generated['attention'].tolist(),
+        'beam': sorted_beam,
+        'greedy': sorted_greedy,
+        'tokens': [token.replace('Ġ', '') for token in generated['tokens']]}
+    return jsonify(return_dict)
 
 @app.route('/', methods=['GET'])
 def verify_online():
