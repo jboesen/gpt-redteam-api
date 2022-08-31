1c266730066e99ca2ac31e4ac4273166ac4a8515
John Boesen <89944638+jboesen@users.noreply.github.com>
2022-08-30 23:35:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from offensive_classifier import sort_offensive
 app = Flask(__name__)
 CORS(app)
 
+print('inmain')
+
 @app.route('/', methods=['POST'])
 def predictions_and_offensiveness():
     """Sorts predictions by offensiveness for redteaming"""
